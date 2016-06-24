67d998ec5bac6fe8e401feae32c4ae27daf0a491
Brian Hans <wbrianw8@gmail.com>
2016-06-24 09:25:40 -0700

    First Version
    Created tip calculator which calculates the tip on bill for 15/18/20%
    tips

--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountTextField: UITextField!
+    @IBOutlet weak var totalAmountTextField: UITextField!
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        guard let billAmount = Double(billAmountField.text!) else{
+            //show error
+            billAmountField.text = ""
+            tipAmountTextField.text = ""
+            totalAmountTextField.text = ""
+            return
+        }
+        
+        var tipPercentage = 0.0
+        switch tipSelector.selectedSegmentIndex {
+        case 0:
+            tipPercentage = 0.15
+        case 1:
+            tipPercentage = 0.18
+        case 2:
+            tipPercentage = 0.2
+        default:
+            break
+        }
+        
+        let roundedBillAmount = round(billAmount * 100)/100
+        let tipAmount = roundedBillAmount * tipPercentage
+        let roundedTipAmount = round(tipAmount * 100)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if (!billAmountField.editing) {
+            billAmountField.text = String(format: "%.2f", roundedBillAmount)
+        }
+        
+        tipAmountTextField.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountTextField.text = String(format: "%.2f", totalAmount)
+    }
 }
 
